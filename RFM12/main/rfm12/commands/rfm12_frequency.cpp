536a9a629b11980c8f9fee5d72cddd98e1d09327
Markus Mayer <development@widemeadows.de>
2013-06-06 11:55:45 +0200

    Frequency Setting Command

---
+++ RFM12/main/rfm12/commands/rfm12_frequency.cpp
@@ -0,0 +1,185 @@
+/*
+ * rfm12_frequency.cpp
+ *
+ * Created: 06.06.2013 11:24:45
+ *  Author: Markus
+ */ 
+
+#include <tgmath.h>
+#include <float.h>
+#include "rfm12_frequency.hpp"
+
+
+/**
+* \brief Sets the target frequency by frequency.
+*
+* \see set_by_channel()
+*
+* \param frequency The target frequency.
+* \param real_frequency The real calculated target frequency.
+* \return true in case of success, false if the frequency was out of range
+*/
+bool rfm12_frequency_command_t::set_by_frequency(const float frequency, float *real_frequency)
+{
+	// f0 = 10*c1*(c2 + frequency/4000)
+	// F  = (400*(frequency - 10*c1*c2))
+	
+	bool valid = true;
+	uint16_t F;
+	
+	if ((frequency >= (430.24 - FLT_EPSILON)) && (frequency <= (439.7575 + FLT_EPSILON)))
+	{
+		// 433MHz band: c1 = 1, c2 = 43
+		F = static_cast<uint16_t>(floor((400.0F*(frequency - 430.0F))));
+		
+		// sanity check
+		if (F < 96) {
+			F = 96;
+			valid = false;
+		}
+		else if (F > 3903) {
+			F = 3903;
+			valid = false;
+		}
+		
+		// calculate real frequency
+		if (NULL != real_frequency) {
+			*real_frequency = 10.0F*(43.0F+F/4000.0F);
+		}
+	}
+	else if ((frequency >= (860.48 - FLT_EPSILON)) && (frequency <= (879.515 + FLT_EPSILON)))
+	{
+		// 868MHz band: c1 = 2, c2 = 43
+		F = static_cast<uint16_t>(floor((200.0F*(frequency - 860.0F))));
+		
+		// sanity check
+		if (F < 96) {
+			F = 96;
+			valid = false;
+		}
+		else if (F > 3903) {
+			F = 3903;
+			valid = false;
+		}
+		
+		// calculate real frequency
+		if (NULL != real_frequency) {
+			*real_frequency = 20.0F*(43.0F+F/4000.0F);
+		}
+	}
+	else if ((frequency >= (900.72 - FLT_EPSILON)) && (frequency <= (929.2725 + FLT_EPSILON)))
+	{
+		// 915MHz band: c1 = 3, c2 = 30
+		F = static_cast<uint16_t>(floor((400.0F*(frequency - 900.0F))/3.0F));
+		
+		// sanity check
+		if (F < 96) {
+			F = 96;
+			valid = false;
+		}
+		else if (F > 3903) {
+			F = 3903;
+			valid = false;
+		}
+		
+		// calculate real frequency
+		if (NULL != real_frequency) {
+			*real_frequency = 30.0F*(30.0F+F/4000.0F);
+		}
+	}
+	else {
+		valid = false;
+		
+		// calculate real frequency
+		if (NULL != real_frequency) {
+			*real_frequency = 0.0F;
+		}
+	}
+	
+	// Set values if successful
+	if (valid) {
+		this->f = F;
+		return true;
+	}
+	return false;
+}
+
+/**
+* \brief Sets the target frequency by channel.
+*
+* \see set_by_frequency()
+*
+* \param channel The target channel.
+* \param real_frequency The real calculated target frequency.
+* \return true in case of success, false if the frequency was out of range
+*/
+bool rfm12_frequency_command_t::set_by_channel(const uint16_t channel, const frequency_band_t band, float *real_frequency)
+{
+	// f0 = 10*c1*(c2 + frequency/4000)
+	// F  = (400*(frequency - 10*c1*c2))
+	
+	bool valid = true;
+	uint16_t F = channel;
+	
+	// sanity check
+	if (channel < 96) {
+		F = 96;
+		valid = false;
+	}
+	else if (channel > 3903) {
+		F = 3903;
+		valid = false;
+	}
+	
+	// select the band
+	switch (band) 
+	{
+		case FREQ_BAND_433: 
+		{
+			// 433MHz band: c1 = 1, c2 = 43
+			
+			// calculate real frequency
+			if (NULL != real_frequency) {
+				*real_frequency = 10.0F*(43.0F+F/4000.0F);
+			}
+			
+			break;
+		}
+		case FREQ_BAND_868: 
+		{
+			// 868MHz band: c1 = 2, c2 = 43
+			
+			// calculate real frequency
+			if (NULL != real_frequency) {
+				*real_frequency = 20.0F*(43.0F+F/4000.0F);
+			}
+			
+			break;
+		}
+		case FREQ_BAND_915: 
+		{
+			// 915MHz band: c1 = 3, c2 = 30
+			
+			// calculate real frequency
+			if (NULL != real_frequency) {
+				*real_frequency = 30.0F*(30.0F+F/4000.0F);
+			}
+			
+			break;
+		}
+		default:
+		{
+			valid = false;
+			if (NULL != real_frequency) {
+				*real_frequency = 0.0F;
+			}
+		}
+	}
+	
+	// Set values if successful
+	if (valid) {
+		this->f = F;
+		return true;
+	}
+	return false;
+}
--- RFM12/main/rfm12/commands/rfm12_frequency.hpp
+++ RFM12/main/rfm12/commands/rfm12_frequency.hpp
@@ -6,15 +6,17 @@
  */ 
 
 
-#ifndef RFM12_POWERMGMT_H_
-#define RFM12_POWERMGMT_H_
+#ifndef RFM12_FREQUENCY_H_
+#define RFM12_FREQUENCY_H_
 
+#include <stdlib.h>
 #include <stdint.h>
+#include "rfm12_configset.hpp"
 
 /**
 * \brief Power Management Command
 */
-typedef class _rfm12_powermgmt_command_t {
+typedef class _rfm12_frequency_command_t {
 	public:
 	union {
 		/**
@@ -25,8 +27,13 @@ typedef class _rfm12_powermgmt_command_t {
 			/**
 			* \brief The command code.
 			*/
-			const uint8_t		command_code:8;		
+			const uint8_t		command_code:4;
 
+
+			/**
+			* \brief The frequency parameter F.
+			*/
+			uint16_t			f:12;
 		};
 	};
 	
@@ -35,15 +42,38 @@ typedef class _rfm12_powermgmt_command_t {
 	/**
 	* \brief Initializes this instance to default values (POR)
 	*/
-	_rfm12_powermgmt_command_t() 
-		: command_word(0x8208)
+	_rfm12_frequency_command_t() 
+		: command_word(0xA680)
 	{}
 
 	/**
-	* \brief Cast-Operator
+	* \brief Cast operator
 	*/
 	inline operator uint16_t() const { return this->command_word; }
 
-} rfm12_powermgmt_command_t;
+	/**
+	* \brief Sets the target frequency by frequency.
+	*
+	* \see set_by_channel()
+	*
+	* \param frequency		The target frequency.
+	* \param real_frequency Optional. The real calculated target frequency.
+	* \return true in case of success, false if the frequency was out of range
+	*/
+	bool set_by_frequency(const float frequency = 433.0F, float *real_frequency = NULL);
 
-#endif /* RFM12_POWERMGMT_H_ */
+	/**
+	* \brief Sets the target frequency by channel.
+	*
+	* \see set_by_frequency()
+	*
+	* \param channel		The target channel.
+	* \param band			The frequency band.
+	* \param real_frequency Optional. The real calculated target frequency. If no valid band was set, this value will be set to zero.
+	* \return true in case of success, false if the channel was out of range
+	*/
+	bool set_by_channel(const uint16_t channel = 1200, const frequency_band_t band = FREQ_BAND_433, float *real_frequency = NULL);
+
+} rfm12_frequency_command_t;
+
+#endif /* RFM12_FREQUENCY_H_ */
