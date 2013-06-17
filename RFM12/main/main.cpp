eafd57327572fe228f24dc118facdff4f244297a
Markus Mayer <development@widemeadows.de>
2013-06-18 00:55:09 +0200

    moved example code to own unit

--- RFM12/main/main.cpp
+++ RFM12/main/main.cpp
@@ -4,9 +4,6 @@
  * Created: 02.06.2013 21:09:43
  *  Author: sunside
  */ 
-	
-#define DEMO_TRANSMITTER_MODE 	true
-#define DEMO_RECEIVER_MODE 		!DEMO_TRANSMITTER_MODE
  
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -20,12 +17,13 @@
 #include "comm/usart_comm.h"
 #include "comm/command_decoder.h"
 
-#include "main.h"
+#include "main.hpp"
 #include "adapters/Rfm12SpiAdapter.hpp"
 #include "adapters/Rfm12ReceiveBuffer.hpp"
 #include "adapters/Rfm12SendBuffer.hpp"
 
 #include "rfm12configuration.hpp"
+#include "demoloops.hpp"
 
 using namespace rfm12;
 using namespace rfm12::commands;
@@ -45,29 +43,7 @@ using namespace rfm12::commands;
 * This flag is driven by the pin change interrupt handler that processes
 * the RFM12's interrupt line.
 */
-static volatile bool _rfm12PulseRequired = false;
-
-/**
-* \brief State of the send demo
-*/
-static enum {
-	RXTXSTATE_IDLE,
-
-#ifdef DEMO_TRANSMITTER_MODE
-
-	RXTXSTATE_IDLE_TXOFF,			//<! System is idle and transmitter is disabled
-	RXTXSTATE_IDLE_TXON,			//<! System is idle and transmitter is enabled
-	RXTXSTATE_TRANSMITTING,			//<! System is transmitting
-
-#else
-	
-	RXTXSTATE_IDLE_RXOFF,			//<! System is idle and receiver is disabled
-	RXTXSTATE_IDLE_RXON,			//<! System is idle and receiver is enabled
-	RXTXSTATE_RECEIVING,			//<! System is receiving
-
-#endif	
-	
-} rxtxdemostate = RXTXSTATE_IDLE;
+volatile bool _rfm12PulseRequired = false;
 
 int main()
 {
@@ -124,133 +100,16 @@ int main()
 #ifdef DEBUG
 	LED_ASSERT(rfm12->getPowerManagementCommand()->checkBitfieldIsValid(), 10);
 #endif	
-	
-#ifdef DEMO_TRANSMITTER_MODE
-	// enable fast-transmitter strategy and commit (transmitter still in idle mode)
-	rfm12->setTransceiverStrategy(RXTXSTRATEGY_FAST_TRANSMITTER, true);
-#else
-	// enable fast-receiver strategy and commit (receiver still in idle mode)
-	rfm12->setTransceiverStrategy(RXTXSTRATEGY_FAST_RECEIVER, true);
-#endif
-	
+		
 	usart_comm_send_zstr("transmitter configured.\r\n");
 	sleep(1);
-
-	// loopity loop.
-	for(;;)
-	{
-		switch (rxtxdemostate) 
-		{
-
-#ifdef DEMO_TRANSMITTER_MODE
-
-			default:
-			case RXTXSTATE_IDLE_TXOFF: 
-			{
-				// at this point, the receiver should not be active, so we may very well reset the buffer
-				rfm12SendBuffer->reset();
-				rfm12SendBuffer->writeSync(0xAA);
-				rfm12SendBuffer->writeSync(0xAA);
-				rfm12SendBuffer->writeSync(0xAA);
-				rfm12SendBuffer->writeSync(0xAA);
-				
-				rfm12SendBuffer->writeSync(0x2D);
-				rfm12SendBuffer->writeSync(0xD4);
-				
-				rfm12SendBuffer->writeSync(0x01);
-				rfm12SendBuffer->writeSync(0x02);
-				rfm12SendBuffer->writeSync(0x03);
-				rfm12SendBuffer->writeSync(0x04);
-				rfm12SendBuffer->writeSync(0x42);
-				rfm12SendBuffer->writeSync(0xB0);
-				rfm12SendBuffer->writeSync(0x0B);
-				rfm12SendBuffer->writeSync(0xE3);
-				
-				rfm12SendBuffer->writeSync(0xAA);
-								
-				// enable transmitter, then sleep
-				rfm12->enterTransmitterMode();
-				usart_comm_send_zstr("transmitter on ...\r\n");
-				// sleep(1);
-				
-				rxtxdemostate = RXTXSTATE_TRANSMITTING;
-				break;
-			}
-						
-			case RXTXSTATE_TRANSMITTING:
-			{			
-				// the interrupt knows best
-				if (_rfm12PulseRequired) {
-					rfm12->pulse();
-					_rfm12PulseRequired = false;
-				}
-				
-				// if the transmission is not done, do not switch state
-				if (!rfm12->isTransmissionDone()) break;
-				usart_comm_send_zstr("data sent.\r\n");
-				rxtxdemostate = RXTXSTATE_IDLE_TXON;
-				break;
-			}
-			
-			case RXTXSTATE_IDLE_TXON:
-			{			
-				// disable transmission
-				rfm12->enterIdleMode();
-				usart_comm_send_zstr("transmitter off.\r\n");
-				
-				// sleep for some time
-				sleep(5);
-				rxtxdemostate = RXTXSTATE_IDLE_TXOFF;
-				break;
-			}
-
+	
+#if DEMO_TRANSMITTER_MODE
+	transmitterDemoLoop(rfm12, rfm12SendBuffer);
 #else
-
-			default:
-			case RXTXSTATE_IDLE_RXOFF: 
-			{
-				// at this point, the receiver should not be active, so we may very well reset the buffer
-				rfm12ReceiveBuffer->reset();
-								
-				// enable transmitter, then sleep
-				rfm12->enterReceiverMode();
-				usart_comm_send_zstr("receiver on ...\r\n");
-				
-				rxtxdemostate = RXTXSTATE_RECEIVING;
-				break;
-			}
-						
-			case RXTXSTATE_RECEIVING:
-			{			
-				// the interrupt knows best
-				if (_rfm12PulseRequired) {
-					set_led(OFF);
-					rfm12->pulse();
-					_rfm12PulseRequired = false;
-				}
-								
-				// if the transmission is not done, do not switch state
-				ringbuffer::rbdata_t item;
-				if (rfm12ReceiveBuffer->tryRead(&item)) {
-					usart_comm_send_char(item);
-				}
-				break;
-			}
-			
-			case RXTXSTATE_IDLE_RXON:
-			{			
-				// disable transmission
-				rfm12->enterIdleMode();
-				usart_comm_send_zstr("receiver off.\r\n");
-
-				rxtxdemostate = RXTXSTATE_IDLE_RXOFF;
-				break;
-			}
-		}
-
+	receiverDemoLoop(rfm12, rfm12ReceiveBuffer);
 #endif
-		}
-	}
+
 }
 
 /*! 
